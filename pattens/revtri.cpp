69d5388abbede045c1ba37b8dcc13381fac5b044
deepanshu <deepuanshu123as@gmail.com>
2025-02-22 18:07:38 +0530

    ge

---
+++ pattens/revtri.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    // square pattern
+    int n;
+    cin >> n;
+    for (int i = 0; i <= n; i++)
+    {
+
+        for (int j = 1; j <= n-i+1; j++)
+        {
+
+            cout << "*";
+        }
+        cout << endl;
+    };
+};
