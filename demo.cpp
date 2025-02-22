70cd2cb7e90f004a65708e4944c816185a574259
deepanshu <deepuanshu123as@gmail.com>
2025-02-22 15:45:42 +0530

    day 2 dsa

---
+++ demo.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int findsum(int i, int k)
+{
+    int sum = i + k;
+    return sum;
+}
+int main()
+{
+    int sum2 = findsum(35, 53);
+    cout << sum2;
+}
